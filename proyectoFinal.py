fcf9427f500b91522229ca28ebf93aa949f08c6d
Daniel Barrantes <djbarrantesrodriguez@gmail.com>
2023-12-03 13:02:01 -0600

    Update proyectoFinal.py
    Código final sin comentarios

--- proyectoFinal.py
+++ proyectoFinal.py
@@ -7,8 +7,10 @@ asientostc=[["1","2"],["3","4"],["5","6"],["7","8"],["9","10"]]
 asientossa=[["1","2"],["3","4"],["5","6"],["7","8"],["9","10"]]
 tiquetes=["Sencillo", "Diario", "Semanal"]
 sel=""
-
-
+ver=False
+id=0
+total = open("total.txt", "w")
+total.close()
 def contarAsientos(ruta):
     dis=0
     for i in range(0,len(ruta),1):
@@ -24,43 +26,90 @@ def printAsientos(ruta):
     print("------------------------------------")
     
 def adquirirAsientos (ruta):
+    global id
+    sub=0
+    iva=0
+    tot=0
+    factura = open(f"factura{id}.txt", "w")
+    factura.close()
+    factura = open(f"factura{id}.txt", "a")
+    factura.write(f"Ruta: {rutas[int(rutas1)-1]}\n")
     tomado=True
-    ver=False
+    global ver
     ver1=False
     print("Estos son los asientos disponibles")
     printAsientos(ruta)
-    sel1=int(input("Digite cuantos asientos desea comprar "))
+    while not ver:
+        try:
+            sel1=int(input("Digite cuantos asientos desea comprar "))
+            ver=True
+        except:
+            print("\nPor favor seleccione una opción válida\n")
+            ver=False
+    ver=False
+    factura.write(f"Tiquetes x{sel1}\n\n")
+    factura.write("*******************************\n\n")
+    
+    
     for k in range(0,sel1,1):
         while not ver:
             sel2=input("Digite el número del asiento que desea ")
-            for i in range(0,len(ruta),1):
-                for j in range(0,len(ruta[0]),1):
-                    if ruta[i][j]==sel2:
-                        while not ver1:
-                            tiq=input("Digite el número del tipo de tiquete que desea adquirir\n1- Sencillo - 500 colones\n2- Diario - 1000 colones\n3- Semanal - 3000 colones\n")
-                            if tiq=="1":
-                                print("Muy bien")
-                                ver1=True
-                            elif tiq=="2":
-                                print("Muy bien")
-                                ver1=True
-                            elif tiq=="3":
-                                print("Muy bien")
-                                ver1=True
-                            else:
-                                print("Muy mal")
-                        ver1=False
-                        ruta[i][j]="O"
-                        tomado=False
-                        ver=True
-            if tomado:
-                print("\nAsiento no disponible")
-            tomado=True
-            print("")
+            try:
+                for i in range(0,len(ruta),1):
+                    for j in range(0,len(ruta[0]),1):
+                        if ruta[i][j]==sel2:
+                            factura.write(f"Asiento {sel2}\n")
+                            while not ver1:
+                                tiq=input("\nDigite el número del tipo de tiquete que desea adquirir\n1- Sencillo - 500 colones\n2- Diario - 1000 colones\n3- Semanal - 3000 colones\n\n")
+                                if tiq=="1":
+                                    factura.write("Sencillo\n")
+                                    factura.write("500 CRC\n\n")
+                                    sub+=500
+                                    ver1=True
+                                elif tiq=="2":
+                                    factura.write("Diario\n")
+                                    factura.write("1000 CRC\n\n")
+                                    sub+=1000
+                                    ver1=True
+                                elif tiq=="3":
+                                    factura.write("Semanal\n")
+                                    factura.write("3000 CRC\n\n")
+                                    sub+=3000
+                                    ver1=True
+                                else:
+                                    print("\nPor favor seleccione una opción válida")
+                            ver1=False
+                            ruta[i][j]="O"
+                            tomado=False
+                            ver=True
+                if tomado:
+                    print("\nAsiento no disponible")
+                tomado=True
+                print("")
+            except:
+                print("Por favor seleccione una opción válida\n")
         ver=False
-        printAsientos(ruta) 
+        printAsientos(ruta)
+    factura.write("*******************************\n\n")
+    iva=sub*0.13
+    tot=sub+iva
     
+    factura.write(f"Subtotal {sub} CRC\n")
+    factura.write(f"IVA - 13% {iva} CRC\n")
+    factura.write(f"Total {tot} CRC")
+    factura.close()
+    id+=1
+    total = open("total.txt", "a")
+    total.write(str(tot)+"\n")
+    total.close()
 
+def sumVentas():
+    tv=0
+    total = open("total.txt", "r")
+    ventas=total.readlines()
+    for i in ventas:
+        tv+=float(i)
+    print(tv)
 
 while sel!="5":
     sel=input("1. Ver Rutas\n2. Ver precios\n3. Adquirir tiquete(s)\n4. Consultar cantidad de espacios disponibles\n5. Salir\n\n")
@@ -79,20 +128,22 @@ while sel!="5":
             print (precios[i])
         print("------------------------------------")
     elif sel=="3":
-        ruta=input("Digite el número de la ruta en la que quiere viajar\n1- Turrialba-San José\n2- Jaco por pista\n3- Concepción Abajo\n4- Turrialba Colectivo\n5- San José-Alajuela\n")
-        if ruta == "1":
+        rutas1=input("Digite el número de la ruta en la que quiere viajar\n1- Turrialba-San José\n2- Jaco por pista\n3- Concepción Abajo\n4- Turrialba Colectivo\n5- San José-Alajuela\n\n")
+        if rutas1 == "1":
             adquirirAsientos(asientosts)
-        elif ruta == "2":
+        elif rutas1 == "2":
             adquirirAsientos(asientosj)
-        elif ruta == "3":
+        elif rutas1 == "3":
             adquirirAsientos(asientosc)
-        elif ruta == "4":
+        elif rutas1 == "4":
             adquirirAsientos(asientostc)
-        elif ruta == "5":
+        elif rutas1 == "5":
             adquirirAsientos(asientossa)
+        else:
+            print("\nOpción no válida")
             
     elif sel=="4":
-        ruta=input("Digite el número de la ruta que quiere revisar\n1- Turrialba-San José\n2- Jaco por pista\n3- Concepción Abajo\n4- Turrialba Colectivo\n5- San José-Alajuela\n")
+        ruta=input("Digite el número de la ruta que quiere revisar\n1- Turrialba-San José\n2- Jaco por pista\n3- Concepción Abajo\n4- Turrialba Colectivo\n5- San José-Alajuela\n\n")
         if ruta == "1":
             printAsientos(asientosts)
             print("Quedan" ,contarAsientos(asientosts), "asientos disponibles")
@@ -108,9 +159,16 @@ while sel!="5":
         elif ruta == "5":
             printAsientos(asientossa)
             print("Quedan" ,contarAsientos(asientosj), "asientos disponibles")
-        
+        else:
+            print("\nPor favor seleccione una opción válida")
+            
     elif sel=="5":
         print("Que tenga un buen día")
+        
+    elif sel=="6":
+        print("Modo Administrador Desbloqueado\n")
+        print("Suma de todas las ventas:")
+        sumVentas()
     else:
         print("Selección no válida")
     input()
